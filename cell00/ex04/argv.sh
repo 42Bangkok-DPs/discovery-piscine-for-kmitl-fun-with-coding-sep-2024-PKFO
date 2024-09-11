391022b584cca5c72d2fec7dcc8c2e8e4eb974db
PKFO <110986886+PKFO@users.noreply.github.com>
2024-09-11 22:04:40 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -2,7 +2,7 @@
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
 fi
-for i in {1..3};  # Loop starts from 0 as arrays are zero-indexed
+for i in {1..3};  
 do
     if [ -n "${!i}" ]; then
         echo "${!i}"
